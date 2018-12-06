5207f22c75c73f3121fe00d32ea3ef8844d4b499
VladimirRudkovskiy <ryan_vancity_crawford@mail.ru>
2018-12-06 19:49:12 +0200

    creating workspace

---
+++ src/components/Layout.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client'
+
+const socketUrl = 'http://192.168.1.100:3231'
+export default class Layout extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state ={
+			socket: null
+		};
+	}
+	componentWillMount() {
+		this.initSocket()
+	}
+
+	initSocket = () => {
+		const socket = io(socketUrl)
+		socket.on('connect', () => {
+			console.log('Connected');
+		})
+		this.setState({socket})
+	}
+
+
+	render() {
+		const { title } = this.props
+		return(
+			<div className='container'>
+			 	{title}
+			</div>
+		);
+	}
+}
